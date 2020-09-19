0c63e441a035a110a92ba8c4086c03d801d15748
Tony69696969 <zxc45693@gmail.com>
2020-09-19 18:27:37 +0800

    “你的备注信息”

---
+++ 2D Stone Age World/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator anim;
+    PlayerController playerController;
+    float lastAttackTime=-10;
+    public float attackCoolDownTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        playerController = GetComponentInParent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("run", Mathf.Abs(playerController.horizontalMove));
+        anim.SetFloat("verticalVelocity", playerController.rb.velocity.y / 10);
+        if (playerController.isOnGround)
+        {
+            anim.SetBool("jump", false);
+        }
+        else
+        {
+            anim.SetBool("jump", true);
+        }
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            if(Time.time >= (lastAttackTime+attackCoolDownTime))
+            {
+                Attack();
+            }
+        }
+    }
+    void Attack()
+    {
+        lastAttackTime = Time.time;
+        anim.SetTrigger("attack");
+    }
+}
+
+//15168
---
+++ 2D Stone Age World/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float horizontalMove;
+    public float moveSpeed;
+    public float jumpForce;
+    public bool jumpPressed;
+    int jumpCount;
+    public bool isOnGround;
+    public float leftFootOffsetX;
+    public float leftFootOffsetY;
+    public float rightFootOffsetX;
+    public float rightFootOffsetY;
+    public float groundDistance;
+    public LayerMask groundLayer;
+    PlayerAnimation playerAnimation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerAnimation = GetComponentInChildren<PlayerAnimation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal");
+        if(Input.GetButtonDown("Jump")&&jumpCount>0)
+        {
+            jumpPressed = true;
+        }
+    }
+    private void FixedUpdate()
+    {
+        PhysicsCheck();
+        GroundMovement();
+        MidAirMovement();
+        FilpDirection();
+    }
+    void GroundMovement()
+    {
+        rb.velocity = new Vector2(horizontalMove * moveSpeed, rb.velocity.y);
+    }
+    void FilpDirection()
+    {
+        if(horizontalMove!=0)
+        {
+            transform.localScale = new Vector3(horizontalMove, 1, 1);
+        }
+    }
+    void MidAirMovement()
+    {
+        if (isOnGround)
+        {
+            jumpCount = 2;
+        }
+        if (jumpPressed && isOnGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+        }
+        else if (jumpPressed && jumpCount > 0 && !isOnGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            jumpCount--;
+            jumpPressed = false;
+        }
+    }
+    
+    void PhysicsCheck()
+    {
+        RaycastHit2D leftCheck = Raycast(new Vector2(leftFootOffsetX, leftFootOffsetY), Vector2.down, groundDistance, groundLayer);
+        RaycastHit2D rightCheck = Raycast(new Vector2(rightFootOffsetX, rightFootOffsetY), Vector2.down, groundDistance, groundLayer);
+        if(leftCheck||rightCheck)
+        {
+            isOnGround = true;
+        }
+        else
+        {
+            isOnGround = false;
+        }
+    }
+    RaycastHit2D Raycast(Vector2 offset,Vector2 rayDiration,float length,LayerMask layer)
+    {
+        Vector2 pos = transform.position;
+        RaycastHit2D hit = Physics2D.Raycast(pos + offset, rayDiration, length, layer);
+        Color color = hit ? Color.red : Color.green;
+        Debug.DrawRay(pos + offset, rayDiration * length,color);
+        return hit;
+    }
+}
